15c1dc5ebb32904c67e880dfcb98aea4b1cb519c
LiubovSokolova89 <liubow.sokolova@gmail.cov>
2022-07-06 11:06:35 +0200

    Bonus miles

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 1000;
+        int rub = 20;
+
+        System.out.println(price / rub + " Бонусных миль");
+
+    }
+}
+
